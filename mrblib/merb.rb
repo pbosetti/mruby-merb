ffffc86bc6f1c32ff84d35b3b7cdc260a77e0b60
Paolo Bosetti <paolo.bosetti@unitn.it>
2013-09-11 15:26:59 +0200

    Major rewrite using reular expressions. This requires the availability of a regex engine (mruby-pcre or mruby-oniguruma)

--- mrblib/merb.rb
+++ mrblib/merb.rb
@@ -1,16 +1,18 @@
 # = MERB -- Ruby Templating
 #
-# Author:: Paolo Bosetti
+# Author:: Paolo Bosetti & Enrico Bertolazzi
 # Copyright (c) 2013 Paolo BOSETTI
 #
 # You can redistribute it and/or modify it under the same terms as Ruby.
 
+# 000000000000000000
 
 class MERB
-  attr_accessor :template, :tags
-  attr_reader :merbout
+
+  attr_reader :commands
+
   def initialize(s = '')
-    @tags     = {open: '<%', close: '%>'}
+    @tags     = { :open => '<%', :close => '%>' }
     @template = s
     @source   = []
     $merbout  = ''
@@ -20,87 +22,147 @@ class MERB
     File.open(in_file, 'r') do |f|
       @template = f.read
     end
-    result = self.run
+    if @template then
+      $merbout = ''
+      self.tokenize
+      self.source
+    end
+
     if out_file then
       File.unlink(out_file) if File.exist? out_file
       File.open(out_file, 'w') do |f|
-        f.write result
+        f.write $merbout
       end
     end
-    return result
-  end
-  
-  def run
-    $merbout = ''
-    self.scan
-    eval self.source
     return $merbout
   end
-  
-  def scan
-    mode = :text
-    chunk = ''
+
+  def tokenize
+    @tokens = []
     raise RuntimeError, "Open and close tags must have the same length!" unless @tags[:open].length == @tags[:close].length
-    tag_l = @tags[:open].length
+    return unless @template.is_a? String
+    state  = [:text]
+    chunk  = ''
+    tag_l  = @tags[:open].length
     window = ' ' * tag_l
+    # inizio scansione
+    # filtro fine linea
+    @template.gsub!( /\r\n/m, "\n" ) ;
+    @template.gsub!( /\r/m, "\n" ) ;
     @template.each_char do |c|
-      next if c == "\r"
-      window = window[1..-1] + c
-      if window == @tags[:open] then
-        @source << [chunk[0..-tag_l], :text]
-        chunk = ''
-        mode = :ruby
-      elsif window == @tags[:close] then
-        @source << [chunk[0..-tag_l], :ruby]
-        chunk = ''
-        mode = :text
-      else
-        case c
-        when '"'
-          chunk << "\""
+      window = window[1..-1] + c # tiene finestra ultimi caratteri letti
+      case state[-1]
+      when :text then
+        if window == @tags[:open] then
+          # cerco di classificare il pezzo di testo
+          tmp = chunk[0..-tag_l] ;
+          if tmp.length > 0 then # se 2 tag consecutivi! niente testo
+            if tmp =~ /^( |\t)*$/ then
+              @tokens << [tmp, :blank]
+            else
+              @tokens << [tmp, :text]
+            end
+          end
+          chunk = ''
+          state << :ruby
+        elsif c == "\n"
+          if chunk =~ /^( |\t)*$/ then
+            @tokens << [chunk, :blank_nl]
+          else
+            @tokens << [chunk, :text_nl]
+          end
+          chunk = ''
         else
           chunk << c
         end
+      when :ruby then
+        if window == @tags[:close] then
+          if chunk[0] == '=' then
+            if chunk[-1] == '-' then
+              @tokens << [chunk[1..-tag_l-1], :ruby_minus]
+            else
+              @tokens << [chunk[1..-tag_l], :ruby]
+            end
+          else
+            @tokens << [chunk[0..-tag_l], :ruby_cmd]
+          end
+          chunk = ''
+          state.pop
+        else
+          state << :ruby_string  if c == "\"" && window[-2] != "\\"
+          state << :ruby_string2 if c == "\'" && window[-2] != "\\"
+          chunk << c
+        end
+      when :ruby_string
+        state.pop if window[-2..-1] != "\\\""
+        chunk << c
+      when :ruby_string2
+        state.pop if window[-2..-1] != "\\\'"
+        chunk << c
       end
     end
     unless chunk.empty? then
-      @source << [chunk, :text]
+      @tokens << [chunk, :text]
     end
   end
   
   def source
-    result = "$merbout = ''; "
-    after_output = false
-    @source.each do |chunk, type|
+    @commands = "$merbout = '';\n"
+    last_tag  = [:null,:null,:null,:null]
+    @commands = []
+    @tokens.each { |chunk, type|
       case type
-      when :text
-        if chunk.match(/^\n\s*$/) && after_output then
-          result << "$merbout.concat \"#{chunk}\"; "
-        elsif ! after_output
-          result << "$merbout.concat \"#{chunk.gsub(/^\n\s*/, "")}\"; " 
+      when :ruby_minus then
+        @commands << "$merbout.concat((#{chunk}).to_s);\n"
+      when :ruby then
+        @commands << "$merbout.concat((#{chunk}).to_s);\n"
+      when :ruby_cmd then
+        @commands << chunk + ";\n"
+      when :text, :text_nl
+        chunk.gsub!( /\\/m, "\\\\" ) ;
+        chunk.gsub!( /\"/m, "\\\"" ) ;
+        chunk.gsub!( /\'/m, "\\\'" ) ;
+        chunk.gsub!( /\`/m, "\\\`" ) ;
+        chunk.gsub!( /\#/m, "\\\#" ) ; # filtra comandi #{}
+        if type == :text then
+          @commands << "$merbout.concat \"#{chunk}\";\n" 
         else
-          result << "$merbout.concat \"#{chunk}\"; " 
+          @commands << "$merbout.concat \"#{chunk}\\n\"; # :text_nl \n" 
         end
-      when :ruby
-        if chunk[0] == '=' then
-          if chunk[-1] == '-' then
-            chunk = chunk[0..-2]
-            after_output = false
-          else
-            after_output = true
-          end
-          result << "$merbout.concat((#{chunk[1..-1]}).to_s); "
-        else
-          after_output = false
-          result << chunk.chomp + ";\n"
-        end
+      when :blank_nl then
+        @commands << "$merbout.concat \"#{chunk}\\n\"; # :blank_nl\n" if last_tag != :ruby_cmd
+      when :blank then
+        @commands << "$merbout.concat \"#{chunk}\"; # :blank\n"
       end
-    end
-    return result
+      last_tag = last_tag[1..-1] << type
+      
+      # {:text_nl,:blank_nl} :blank :ruby_cmd :blank_nl --> {:text_nl,:blank_nl} :ruby_cmd
+      # {:text_nl,:blank_nl} :ruby_cmd :blank_nl        --> {:text_nl,:blank_nl} :ruby_cmd
+      if last_tag == [:text_nl,:blank,:ruby_cmd,:blank_nl] then
+        last_tag = [:null,:null,:null,:blank_nl]
+        @commands.pop
+        tmp = @commands.pop
+        @commands.pop
+        @commands << tmp
+      elsif last_tag == [:blank_nl,:blank,:ruby_cmd,:blank_nl] then
+        last_tag = [:null,:null,:null,:blank_nl]
+        @commands.pop
+        tmp = @commands.pop
+        @commands.pop
+        @commands << tmp
+      elsif last_tag[1..-1] == [:text_nl,:ruby_cmd,:blank_nl] then
+        last_tag = [:null,:null,:null,:blank_nl]
+        @commands.pop
+      elsif last_tag[1..-1] == [:blank_nl,:ruby_cmd,:blank_nl] then
+        last_tag = [:null,:null,:null,:blank_nl]
+        @commands.pop
+      end
+    }
+    eval @commands.join
   end
 end
 
-
+# 000000000000000000
 
 if __FILE__ == $0 then
   
